4ad941a55aa2c412d189911940372ab6c850f1f0
Kyle Zarif <kyle.zarif@gmail.com>
2020-02-29 21:38:10 -0600

    training, test, and validation sets

--- Energy_Efficiency.R
+++ Energy_Efficiency.R
@@ -18,5 +18,43 @@ colnames(eefm) <- c("Relative_Compactness","Surface_Area","Wall_Area",
                     "Roof_Area", "Overall_Height", "Orientation", "Glazing_Area",
                     " Glazing_Area_Distribution", "Heating_Load", "Cooling_Load")
 
+# Validation set will be 10% of dpp data
+set.seed(1, sample.kind="Rounding")
+# if using R 3.5 or earlier, use `set.seed(1)` instead
+test_index <- createDataPartition(y = eefm$Heating_Load, times = 1, p = 0.1, list = FALSE)
+ee <- eefm[-test_index,]
+validation <- eefm[test_index,]
 
-head(eefm)
+
+########################
+#Load Required Libraries
+########################
+library(dslabs)
+library(tidyverse)
+library(caret)
+library(lubridate)
+library(purrr)
+library(knitr)
+library(dplyr)
+
+##################
+# Data Exploration
+##################
+
+# Dimensions of the dp set and test set and checking the variables
+dim(ee)
+dim(validation)
+head(ee)
+head(validation)
+
+###################################################
+# Dividing the dp data into training and test set
+###################################################
+
+set.seed(1, sample.kind="Rounding")
+test_index <- createDataPartition(y = ee$Heating_Load, times = 1, p = 0.2, list = FALSE)
+train_set <- ee[-test_index,]
+test_set <- ee[test_index,]
+
+dim(train_set)
+dim(test_set)
